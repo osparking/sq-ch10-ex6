01c34dd8200783c6662636569be76b0ac29515a1
osparking <jbpark03@gmail.com>
2023-10-18 14:54:34 +0900

    initial - 2 dependencies

---
+++ src/main/java/sqch10ex6/SqCh10Ex6Application.java
@@ -0,0 +1,13 @@
+package sqch10ex6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh10Ex6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh10Ex6Application.class, args);
+	}
+
+}
---
+++ src/test/java/sqch10ex6/SqCh10Ex6ApplicationTests.java
@@ -0,0 +1,13 @@
+package sqch10ex6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh10Ex6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
