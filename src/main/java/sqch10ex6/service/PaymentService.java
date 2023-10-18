d6edb811e837f5df6653d3b6701696cfcf21a15e
osparking <jbpark03@gmail.com>
2023-10-18 21:46:54 +0900

    set exception message in a service method

--- src/main/java/sqch10ex6/service/PaymentService.java
+++ src/main/java/sqch10ex6/service/PaymentService.java
@@ -8,6 +8,8 @@ import sqch10ex6.exception.NotEnoughMoneyException;
 @Service
 public class PaymentService {
 	public PaymentDetails processPayment() throws NotEnoughMoneyException{
-		throw new NotEnoughMoneyException();
+		var e = new NotEnoughMoneyException();
+		e.setMessage("계좌에 잔고가 부족합니다.");
+		throw e;
 	}
 }
